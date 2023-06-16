b8bd15db25cde5919255c2c19b420cd92b51f121
MustapheMS <moztaphem9@gmail.com>
2023-06-16 12:02:45 +0300

    Sum Two Smallest Numbers Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+
+//Done 💯💯👍👍
+
+function sumTwoSmallestNums(arr) {
+  arr.sort(function(c, d) {
+    return c - d;
+  });
+  var sum = arr[0] + arr[1];
+  return sum;
+}
+console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])); 
+console.log(sumTwoSmallestNums([190, 300, 230, 800])); 
+console.log(sumTwoSmallestNums([200, 300, 100, 400])); 
